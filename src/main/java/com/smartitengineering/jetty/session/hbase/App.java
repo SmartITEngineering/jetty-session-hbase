8d88d41320ba2b62dd7c775c6cea4c1bdc0bbd2f
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-11-16 08:51:08 +0600

    Add base project for Jetty session replication through HBase
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ src/main/java/com/smartitengineering/jetty/session/hbase/App.java
@@ -0,0 +1,13 @@
+package com.smartitengineering.jetty.session.hbase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
