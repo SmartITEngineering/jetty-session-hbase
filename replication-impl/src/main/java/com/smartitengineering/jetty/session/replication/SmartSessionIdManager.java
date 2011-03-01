8fe668ce3e3b74a5fcc52a3739dc1d52ef7dcdb2
Imran M Yousuf <imyousuf@smartitengineering.com>
2011-03-01 12:28:28 +0600

    Add session ID cache
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- replication-impl/src/main/java/com/smartitengineering/jetty/session/replication/SmartSessionIdManager.java
+++ replication-impl/src/main/java/com/smartitengineering/jetty/session/replication/SmartSessionIdManager.java
@@ -22,6 +22,8 @@ package com.smartitengineering.jetty.session.replication;
 import com.smartitengineering.jetty.session.replication.SmartSessionManager.Session;
 import java.util.Date;
 import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.locks.ReentrantLock;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import org.eclipse.jetty.server.Handler;
@@ -30,6 +32,7 @@ import org.eclipse.jetty.server.SessionManager;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.session.AbstractSessionIdManager;
 import org.eclipse.jetty.server.session.SessionHandler;
+import org.eclipse.jetty.util.ConcurrentHashSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,7 +44,8 @@ public class SmartSessionIdManager extends AbstractSessionIdManager {
 
   protected final Logger logger = LoggerFactory.getLogger(getClass());
   private final Server server;
-  private final Object lockObject = new Object();
+  private final ReentrantLock lock = new ReentrantLock();
+  private final Set<String> sessionIds = new ConcurrentHashSet<String>();
 
   public SmartSessionIdManager(Server server, Random random) {
     super(random);
@@ -55,7 +59,23 @@ public class SmartSessionIdManager extends AbstractSessionIdManager {
   @Override
   public boolean idInUse(String id) {
     logger.info("idInUse");
-    return SessionReplicationAPI.getInstance().getIdReader().getById(id) != null;
+    if (id == null) {
+      return false;
+    }
+    String clusterId = getClusterId(id);
+    boolean inUse = false;
+    inUse = sessionIds.contains(clusterId);
+    if (inUse) {
+      return true; //optimisation - if this session is one we've been managing, we can check locally
+    }
+    //otherwise, we need to go to the database to check
+    try {
+      return SessionReplicationAPI.getInstance().getIdReader().getById(id) != null;
+    }
+    catch (Exception e) {
+      logger.warn("Problem checking inUse for id=" + clusterId, e);
+      return false;
+    }
   }
 
   @Override
@@ -64,40 +84,60 @@ public class SmartSessionIdManager extends AbstractSessionIdManager {
     if (httpSession == null || !(httpSession instanceof Session)) {
       return;
     }
-    Session session = (Session) httpSession;
+    lock.lock();
     try {
+      Session session = (Session) httpSession;
       SessionId sessionId = new SessionId();
       sessionId.setCreatedAt(new Date().getTime());
-      sessionId.setId(session.getClusterId());
+      final String id = session.getClusterId();
+      sessionId.setId(id);
+      if (logger.isInfoEnabled()) {
+        logger.info("Session id " + sessionId.getId() + " " + sessionId.getCreatedAt());
+      }
       SessionReplicationAPI.getInstance().getIdWriter().save(sessionId);
+      sessionIds.add(id);
     }
     catch (Exception ex) {
       logger.error("Could not add session id!", ex);
+    }
+    finally {
+      lock.unlock();
     }
   }
 
   @Override
   public void removeSession(HttpSession httpSession) {
     logger.info("removeSession");
-    if (httpSession == null || !(httpSession instanceof Session)) {
-      return;
+    lock.lock();
+    try {
+      if (httpSession == null || !(httpSession instanceof Session)) {
+        return;
+      }
+      Session session = (Session) httpSession;
+      final String clusterId = session.getClusterId();
+      removeSession(clusterId);
     }
-    Session session = (Session) httpSession;
-    final String clusterId = session.getClusterId();
-    removeSession(clusterId);
+    finally {
+      lock.unlock();
+    }
   }
 
   protected void removeSession(final String clusterId) {
     logger.info("removeSession");
+    lock.lock();
     try {
       SessionId sessionId =
                 SessionReplicationAPI.getInstance().getIdReader().getById(clusterId);
       if (sessionId != null) {
         SessionReplicationAPI.getInstance().getIdWriter().delete(sessionId);
+        sessionIds.remove(sessionId.getId());
       }
     }
     catch (Exception ex) {
       logger.error("Could not remove session id!", ex);
+    }
+    finally {
+      lock.unlock();
     }
   }
 
@@ -105,9 +145,9 @@ public class SmartSessionIdManager extends AbstractSessionIdManager {
   public void invalidateAll(String id) {
     logger.info("invalidateAll");
     //take the id out of the list of known sessionids for this node
-    removeSession(id);
-
-    synchronized (lockObject) {
+    lock.lock();
+    try {
+      removeSession(id);
       //tell all contexts that may have a session object with this id to
       //get rid of them
       Handler[] contexts = server.getChildHandlersByClass(ContextHandler.class);
@@ -123,6 +163,9 @@ public class SmartSessionIdManager extends AbstractSessionIdManager {
         }
       }
     }
+    finally {
+      lock.unlock();
+    }
   }
 
   @Override
@@ -135,8 +178,8 @@ public class SmartSessionIdManager extends AbstractSessionIdManager {
   @Override
   public String getNodeId(String clusterId, HttpServletRequest request) {
     logger.info("getNodeId");
-    if (_workerName != null) {
-      return clusterId + '.' + _workerName;
+    if (getWorkerName() != null) {
+      return clusterId + '.' + getWorkerName();
     }
 
     return clusterId;
